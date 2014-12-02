4c8ca2ee3715cb4c3c59fb7ad7209dc792f6feba
Jon Higgs <jhiggs@eml.cc>
2014-12-02 20:26:14 +1100

    Rearrange some things.

--- guess_number.py
+++ guess_number.py
@@ -6,9 +6,6 @@ COMPUTER_GUESS = random.randint(1,20)
 TRIES = 6
 NAME = raw_input("What is your name? ")
 
-print("Hello %s! Please guess the number between 1 and 20." %(NAME) )
-print("You get %s tries" %(TRIES) )
-
 def guess(number, attempt):
     if number < COMPUTER_GUESS:
         print("Too low!")
@@ -19,6 +16,10 @@ def guess(number, attempt):
     if number == COMPUTER_GUESS:
         print("YESSSSS %s, you won, and it only took you %s guesses" %(NAME,attempt))
         exit()
+
+
+print("Hello %s! Please guess the number between 1 and 20." %(NAME) )
+print("You get %s tries" %(TRIES) )
 
 for attempt in xrange(TRIES):
     guess(int(raw_input("take a guess: ")), attempt+1)
