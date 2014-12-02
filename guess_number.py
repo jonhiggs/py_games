2ba172062d33edacebaeb14a28e73e13062b56f1
Jon Higgs <jhiggs@eml.cc>
2014-12-02 20:23:30 +1100

    Name tries a constant.

--- guess_number.py
+++ guess_number.py
@@ -4,13 +4,13 @@ from sys import exit
 
 COMPUTER_GUESS = 3 #random.randint(1,20)
 guess_total = 0
-tries = 6
+TRIES = 6
 
 # TODO: name should be constant.
 NAME = raw_input("What is your name? ")
 
 print("Hello %s! Please guess the number between 1 and 20." %(NAME) )
-print("You get %s tries" %(tries) )
+print("You get %s tries" %(TRIES) )
 
 def guess(number, attempt):
     if number < COMPUTER_GUESS:
@@ -23,7 +23,7 @@ def guess(number, attempt):
         print("YESSSSS %s, you won, and it only took you %s guesses" %(NAME,attempt))
         exit()
 
-for attempt in xrange(tries):
+for attempt in xrange(TRIES):
     guess(int(raw_input("take a guess: ")), attempt+1)
 
 print("You lose, the number I was thinking of was %s" %(COMPUTER_GUESS) )
