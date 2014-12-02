1180d999ae713b4c54086271f535328256f960ee
Jon Higgs <jhiggs@eml.cc>
2014-12-02 20:20:39 +1100

    Break the guessing into a method.

--- guess_number.py
+++ guess_number.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 import random
+from sys import exit
 
 COMPUTER_GUESS = random.randint(1,20)
 guess_total = 0
 tries = 6
 
+# TODO: name should be constant.
 name = raw_input("What is your name? ")
 
 print("Hello %s! Please guess the number between 1 and 20." %(name) )
 print("You get %s tries" %(tries) )
 
-while guess_total < tries :
-    guess = int(raw_input("take a guess: " ))
-    guess_total += 1
-
-    if guess < COMPUTER_GUESS:
+def guess(number, attempt):
+    if number < COMPUTER_GUESS:
         print("Too low!")
-    elif guess > COMPUTER_GUESS:
-        print("Too high!")
-    elif guess == COMPUTER_GUESS:
-        break
-    else:
-        print("oops, something went wrong")
 
-if guess == COMPUTER_GUESS:
-    print("YESSSSS %s, you won, and it only took you %s guesses" %(name,guess_total))
-else:
-    print("You lose, the number I was thinking of was %s" %(COMPUTER_GUESS) )
+    if number > COMPUTER_GUESS:
+        print("Too high!")
+
+    if number == COMPUTER_GUESS:
+        print("YESSSSS %s, you won, and it only took you %s guesses" %(name,attempt))
+        exit()
+
+for attempt in xrange(tries):
+    guess(int(raw_input("take a guess: ")), attempt)
+
+print("You lose, the number I was thinking of was %s" %(COMPUTER_GUESS) )
